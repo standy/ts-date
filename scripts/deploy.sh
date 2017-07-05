09ea5c12f3cfe8e6696ee8cbc2654da76816d768
standy <mostovoyav@gmail.com>
2017-07-06 02:46:34 +0300

    chore: fix travis deploy to use !/bin/sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Deploy for tag: '$1'";
-if  [[ $1 =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]
+if echo "$1" | grep -Eq "^v[0-9]+\.[0-9]+\.[0-9]+$"
 then
     npm publish ./npm;
     npm run coveralls;
