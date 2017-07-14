19c0c8f60502050746f5de4994bdedd21b4c66b5
standy <mostovoyav@gmail.com>
2017-07-15 02:33:19 +0300

    perf: caching in format method

--- src/format/create-format.ts
+++ src/format/create-format.ts
@@ -1,25 +1,49 @@
 import {FormatByTemplateFn} from '../utils/basic-types';
 import {tokensRx} from '../utils/tokens-rx';
-import {FormatterObj} from './default-formatters';
+import {Formatter, FormatterObj} from './default-formatters';
 import {isValidDate} from '../default-exports';
 
-export function createFormat(formatters: FormatterObj): FormatByTemplateFn {
+type Token = string | Formatter;
+
+function splitToTokens(template: string, formatters: FormatterObj): Token[] {
 	const RX_TOKENS = tokensRx(Object.keys(formatters));
+	const tokens = template.match(RX_TOKENS) as string[];
+	// this regexp cant fail because of "|."
 
-	return (d: ValidDate | Date | null, template: string): any => {
+	const result: Token[] = [];
+	for (let i = 0; i < tokens.length; i++) {
+		const token = tokens[i];
+
+		const tokenFn = formatters[token];
+		if (tokenFn) {
+			result.push(tokenFn);
+		} else if (token.charAt(0) === '[' && token.charAt(token.length - 1) === ']') {
+			result.push(token.substring(1, token.length - 1));
+		} else {
+			result.push(token);
+		}
+	}
+	return result;
+}
+
+function formatByTokens(d: ValidDate, tokens: Token[]): string {
+	const result: (string | number)[] = [];
+	for (let i = 0; i < tokens.length; i++) {
+		const token = tokens[i];
+		result.push(typeof token === 'function' ? token(d) : token);
+	}
+	return result.join('');
+}
+
+export function createFormat(formatters: FormatterObj): FormatByTemplateFn {
+	const cache: {[template: string]: Token[]} = {};
+	return (d: Date | null, template: string): any => {
 		if (!isValidDate(d)) return null;
-		const tokens = template.match(RX_TOKENS) as string[];
-		// this regexp cant fail because of "|."
-
-		return tokens.map(token => {
-			const tokenFn = formatters[token];
-			if (tokenFn) {
-				return tokenFn(d as ValidDate);
-			}
-			if (token.charAt(0) === '[' && token.charAt(token.length - 1) === ']') {
-				return token.substring(1, token.length - 1);
-			}
-			return token;
-		}).join('');
+		let tokens = cache[template];
+		if (tokens === undefined) {
+			tokens = splitToTokens(template, formatters);
+			cache[template] = tokens;
+		}
+		return formatByTokens(d, tokens);
 	};
 }
