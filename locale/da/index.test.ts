8b0d7fae23d489879c114d7216a4d49d1e4f5b2e
Nill Mortensen <nim@dff-edb.dk>
2019-07-19 11:30:49 +0200

    feat: da locale

---
+++ locale/da/index.test.ts
@@ -0,0 +1,80 @@
+import {Month} from '../../src/utils/basic-types';
+import * as assert from 'power-assert';
+import {newValidDate} from '../../src/create/create-ts-date';
+import {format, parse} from './index';
+
+describe('da locale', function() {
+	it('correct formatting', function() {
+		const date = newValidDate(2017, Month.Oct, 1, 12, 34, 56, 789);
+		const FORMATS = [
+			['MMMM MMM dd ddd dddd A Do Wo Mo DDDo do Qo', 'oktober okt sø søn søndag PM 1. 39. 10. 274. 0. 4.'],
+			['D MMMM hh A', '1 oktober 12 PM'],
+			['Do MMMM, dddd', '1. oktober, søndag'],
+			['D MMM, dd', '1 okt, sø'],
+			['D MMMM, hh A, ddd', '1 oktober, 12 PM, søn'],
+			['Qo [Qo], Mo [Mo]', '4. Qo, 10. Mo'],
+		];
+
+		for (let i = 0; i < FORMATS.length; i++) {
+			const [template, correctResult] = FORMATS[i];
+			const result = format(date, template);
+			assert.equal(result, correctResult, `format "${template}"`);
+		}
+	});
+	it('correct ordinal', function() {
+		const d1 = newValidDate(2017, Month.Oct, 12);
+		const d2 = newValidDate(2017, Month.Oct, 22);
+		const d3 = newValidDate(2017, Month.Oct, 23);
+		assert.equal(format(d1, 'Do MMMM'), '12. oktober');
+		assert.equal(format(d2, 'Do MMMM'), '22. oktober');
+		assert.equal(format(d3, 'Do MMMM'), '23. oktober');
+	});
+	it('correct day part format', function() {
+		const d1 = newValidDate(2017, Month.Oct, 1, 0, 42, 12);
+		const d2 = newValidDate(2017, Month.Oct, 1, 18, 42, 12);
+		assert.equal(format(d1, 'h A'), '12 AM');
+		assert.equal(format(d2, 'h A'), '6 PM');
+		assert.equal(format(d1, 'h a'), '12 am');
+		assert.equal(format(d2, 'h a'), '6 pm');
+		assert.equal(format(d1, 'h aa'), '12 a.m.');
+		assert.equal(format(d2, 'h aa'), '6 p.m.');
+		assert.equal(format(d1, 'HH:mm:ss'), '00:42:12');
+		assert.equal(format(d2, 'HH:mm:ss'), '18:42:12');
+	});
+	it('correct parsing', function() {
+		const FORMATS = [
+			{
+				template: 'D MMMM YY [Year]',
+				dateStr: '1 oktober 17 Year',
+				correctResult: newValidDate(2017, Month.Oct, 1),
+			},
+			{
+				template: 'YYYY MMMM',
+				dateStr: '2017 OKTOBER',
+				correctResult: newValidDate(2017, Month.Oct, 1),
+			},
+			{
+				/* NOTE Wrong format*/
+				template: 'YYYY MMMM',
+				dateStr: '2017 okt',
+				correctResult: null,
+			},
+			{
+				template: 'Do MMM YY',
+				dateStr: '1. okt 17',
+				correctResult: newValidDate(2017, Month.Oct, 1),
+			},
+			{
+				template: 'MMMM YYYY',
+				dateStr: 'oktober 2017',
+				correctResult: newValidDate(2017, Month.Oct, 1),
+			},
+		];
+
+		for (let i = 0; i < FORMATS.length; i++) {
+			const {template, dateStr, correctResult} = FORMATS[i];
+			const result = parse(dateStr, template);
+			assert.deepEqual(result, correctResult, `parse "${dateStr}" with "${template}"`);
+		}
+	});
+});
---
+++ locale/da/index.ts
@@ -0,0 +1,112 @@
+import {createCustomFormatFn, createFormat} from '../../src/format/create-format';
+import {createParse, parseOrThrowWrapper} from '../../src/parse/create-parse';
+import {defaultFormatters} from '../../src/format/default-formatters';
+import defaultParsers from '../../src/parse/default-parsers';
+import {
+	ParserObj,
+	FormatterObj,
+	FormatByTemplateFn,
+	ParseByTemplateFn,
+	ParseByTemplateOrThrowFn,
+} from '../../src/utils/basic-types';
+import {extend} from '../../src/utils/utils';
+export * from '../../src/default-exports';
+
+// Note: in Danish, the names of days of the week and months are lowercased.
+const months3char = ['jan', 'feb', 'mar', 'apr', 'maj', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'dec'];
+const monthsFull = [
+	'januar',
+	'februar',
+	'marts',
+	'april',
+	'maj',
+	'juni',
+	'juli',
+	'august',
+	'september',
+	'oktober',
+	'november',
+	'december',
+];
+const weekdays2char = ['sø', 'ma', 'ti', 'on', 'to', 'fr', 'lø'];
+const weekdays3char = ['søn', 'man', 'tir', 'ons', 'tor', 'fre', 'lør'];
+const weekdaysFull = ['søndag', 'mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag'];
+const meridiemUppercase = ['AM', 'PM'];
+const meridiemLowercase = ['am', 'pm'];
+const meridiemFull = ['a.m.', 'p.m.'];
+
+export const formatters: FormatterObj = {
+	// Month: jan, feb, ..., dec
+	MMM: date => months3char[date.getMonth()],
+
+	// Month: januar, februar, ..., december
+	MMMM: date => monthsFull[date.getMonth()],
+
+	// Day of week: sø, ma, ..., lø
+	dd: date => weekdays2char[date.getDay()],
+
+	// Day of week: søn, man, ..., lør
+	ddd: date => weekdays3char[date.getDay()],
+
+	// Day of week: søndag, mandag, ..., lørdag
+	dddd: date => weekdaysFull[date.getDay()],
+
+	// AM, PM
+	A: date => meridiemUppercase[date.getHours() < 12 ? 0 : 1],
+
+	// am, pm
+	a: date => meridiemLowercase[date.getHours() < 12 ? 0 : 1],
+
+	// a.m., p.m.
+	aa: date => meridiemFull[date.getHours() < 12 ? 0 : 1],
+};
+
+// Generate ordinal version of formatters: M -> Mo, D -> Do, etc.
+const ordinalFormatters = ['M', 'D', 'DDD', 'd', 'Q', 'W'];
+ordinalFormatters.forEach(formatterToken => {
+	formatters[formatterToken + 'o'] = date => ordinal(defaultFormatters[formatterToken](date) as number);
+});
+
+function ordinal(number: number) {
+	return number + '.';
+}
+
+function toLower(s: string) {
+	return s.toLowerCase();
+}
+const months3charLower = months3char.map(toLower);
+const monthsFullLower = monthsFull.map(toLower);
+const parsers: ParserObj = {
+	// Month: jan, feb, ..., dec
+	MMM: [
+		months3charLower.join('|'),
+		(date, value) => {
+			const index = months3charLower.indexOf(value.toLowerCase());
+			date.setMonth(index);
+		},
+	],
+
+	// Month: januar, februar, ..., december
+	MMMM: [
+		monthsFullLower.join('|'),
+		(date, value) => {
+			value = value.toLowerCase();
+			let index = monthsFullLower.indexOf(value);
+			date.setMonth(index);
+		},
+	],
+};
+
+ordinalFormatters.forEach(formatterToken => {
+	parsers[formatterToken + 'o'] = [
+		'\\d+(?:[.])',
+		(date, value) => {
+			defaultParsers[formatterToken][1](date, value.slice(0, -1));
+		},
+	];
+});
+
+export const format: FormatByTemplateFn = createFormat(extend(defaultFormatters, formatters));
+export const createCustomFormat = createCustomFormatFn(extend(defaultFormatters, formatters));
+export const parse: ParseByTemplateFn = createParse(extend(defaultParsers, parsers));
+export const parseOrThrow: ParseByTemplateOrThrowFn = parseOrThrowWrapper(parse);
