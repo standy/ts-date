51fc8d660b9d8695423b084ef30021724d27d356
standy <mostovoyav@gmail.com>
2020-02-10 17:31:07 +0300

    test: script for updating benchmark

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,3 +1,7 @@
+console.log('Performance test compared to `momentjs` and `date-fns`');
+console.log(' ');
+console.log('```');
+
 require('./log-versions');
 require('./suites/add-days');
 require('./suites/add-hours');
@@ -8,3 +12,4 @@ require('./suites/format');
 require('./suites/format-iso');
 require('./suites/parse-iso');
 
+console.log('```');
