baf40c9ef4ed0de99af0a61a11d4b770a4d92f6b
standy <mostovoyav@gmail.com>
2020-07-23 16:31:43 +0300

    fix: type error with formatDateIso points to null for ValidDate

--- src/format/format-iso.ts
+++ src/format/format-iso.ts
@@ -1,21 +1,22 @@
 import {FormatFn} from '../utils/basic-types';
 import {isValidDate} from '../create/create-ts-date';
 import {leadZero} from '../utils/utils';
+import {ValidDate} from '../valid-date';
 
 // YYYY-MM-DD
-export function formatDateIso<T>(date: Date | null): FormatFn<T> {
+export function formatDateIso<T extends ValidDate | Date | null>(date: T): FormatFn<T> {
 	if (!isValidDate(date)) return null as any;
 	return leadZero(date.getFullYear(), 4) + '-' + leadZero(date.getMonth() + 1) + '-' + leadZero(date.getDate());
 }
 
 // YYYY-MM-DDTHH:mm
-export function formatDateTimeIso<T>(date: Date | null): FormatFn<T> {
+export function formatDateTimeIso<T extends ValidDate | Date | null>(date: T): FormatFn<T> {
 	if (!isValidDate(date)) return null as any;
 	return formatDateIso(date) + 'T' + leadZero(date.getHours()) + ':' + leadZero(date.getMinutes());
 }
 
 // YYYY-MM-DDTHH:mm:ss.SSS
-export function formatLocalIso<T>(date: Date | null): FormatFn<T> {
+export function formatLocalIso<T extends ValidDate | Date | null>(date: T): FormatFn<T> {
 	if (!isValidDate(date)) return null as any;
 	return formatDateTimeIso(date) + ':' + leadZero(date.getSeconds()) + '.' + leadZero(date.getMilliseconds(), 3);
 }
